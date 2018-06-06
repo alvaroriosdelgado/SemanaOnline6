bfbed2bf071aebeb65013f93e0441e66c6c32e2c
Alvaro Rios <alvaro@smartsites.cl>
2018-06-05 21:47:08 -0400

    Adding for and times cicles

--- E6CP1A1/2 ciclos/ejercicio3.rb
+++ E6CP1A1/2 ciclos/ejercicio3.rb
@@ -2,7 +2,18 @@
 # while, for, times.
 
 i = 1
+
  while i <= 990
    puts i if (990 % i).zero?
    i += 1
  end
+
+990.times do
+  puts i if (990 % i).zero?
+  i += 1
+end
+
+for i in 1..990
+  puts i if (990 % i).zero?
+  i += 1
+end
