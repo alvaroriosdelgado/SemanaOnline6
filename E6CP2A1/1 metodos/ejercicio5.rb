f3ae2c15b745f7854490e6d0ec029a815bfaa312
Alvaro Rios <alvaro@smartsites.cl>
2018-06-07 21:07:32 -0400

    Fixing Exercise 5

---
+++ E6CP2A1/1 metodos/ejercicio5.rb
@@ -0,0 +1,9 @@
+# Crear un método que reciba como parámetro dos números enteros positivos
+# e imprima los números pares que existen entre esos dos números.
+
+def pares(num1, num2)
+  (num1..num2).each do |i|
+    puts i if i.even?
+  end
+end
+pares(2, 8)
